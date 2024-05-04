b08973f8d0335535c8a026b10c1d95f2988856d6
Benyamin Gheiji <benyamingheiji@gmail.com>
2024-05-05 03:04:49 +0330

    no message

--- app.py
+++ app.py
@@ -8,14 +8,14 @@ columns= [['Age', 'Infarction volume', 'Comorbidity']]
 
 
 
-model = joblib.load('DC3.pkl')
+model = joblib.load('DC_3m.pkl')
 
 
 st.title('Prediction of 3 month mortality of patients undergoing DC :brain:')
 
 age = st.number_input("Enter the patient age")
 mrs = st.number_input("Enter the MRS of patients while discharging")
-comorbidity= st.selectbox('Choose the comorbidity? 1:Diabetes,2:Hypertension,3:Ischemic Heart Disease,4:Coagulopathy disorder,5:unknown,6:negative,7:Pneumonia,8:Metabolic Syndrome,9:cancer,10:cerebral vascular accident'
+comorbidity= st.selectbox('Choose the comorbidity? Diabetes(1),Hypertension(2),Ischemic Heart Disease(3),Coagulopathy disorder(4),unknown(5),negative(6),Pneumonia(7),Metabolic Syndrome(8),cancer(9),cerebral vascular accident(10)',
                        [1,2,3,4,5,6,7,8,9,10])
 
 
@@ -23,11 +23,10 @@ comorbidity= st.selectbox('Choose the comorbidity? 1:Diabetes,2:Hypertension,3:I
 def predict(): 
     row = np.array([age,mrs,comorbidity]) 
     X = pd.DataFrame([row], columns = columns)
-    X= pd.get_dummies(columns=['Comorbidity'])
     prediction = model.predict_proba(X)
-    if prediction[0] >= 0.35: 
+    if prediction[0][1] >= 0.35: 
         st.error('The patients is more likely not to survive :thumbsdown:')
-    elif prediction[0] < 0.35:
+    elif prediction[0][1] < 0.35:
         st.success('The patients is more likely to survive :thumbsup:') 
         
 
